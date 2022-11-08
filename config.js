1de91d5b7b901dfc56c06db30077506925a9d0c2
satriazy <117642014+satriazy@users.noreply.github.com>
2022-11-08 22:28:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['66827294199','62895618225834','6283892675760']
+global.owner = ['66827294199','6281371462770','6282276049610']
 global.premium = ['66827294199']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
